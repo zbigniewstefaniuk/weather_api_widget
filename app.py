079bef27aefa023149ef97165d14c41c0988fde7
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-09-10 18:45:51 +0200

    added rounded values of temperature

--- app.py
+++ app.py
@@ -10,19 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    city = 'Zakopane'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=0cecf8365f48c0900489013ff0623926'
-
-    response = requests.get(url).json()
-
-    weather = {
-        'city': city,
-        'temperature': response['main']['temp'],
-        'description': response['weather'][0]['description'],
-        'icon': response['weather'][0]['icon'],
-    }
-
-    return render_template('index.html', weather=weather)
+    return render_template('index.html')
 
 
 @app.route('/weather', methods=['GET', 'POST'])
@@ -40,7 +28,10 @@ def weather():
         'icon': response['weather'][0]['icon'],
     }
 
-    return render_template('weather.html', weather=weather)
+    temp_float = weather.get('temperature')
+    temp_int = round(temp_float)
+
+    return render_template('weather.html', weather=weather, temp=temp_int)
 
 
 if __name__ == '__main__':
