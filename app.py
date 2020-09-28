0eba297eb56b2c20dc6e6732c1982f9c3b1a273f
zbigniewstefaniuk <zbyszek791@hotmail.com>
2020-09-28 02:24:35 +0200

    added basic hour forecast

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     API_KEY = '0cecf8365f48c0900489013ff0623926'
-    
+
     # when user first time hits site, showing him Kraków Weather
     try:
         user_input = request.form.get('user_city')
@@ -76,11 +76,57 @@ def index():
         return temp_night
 
     day_forecast = day_forecast()
-    print(day_forecast)
     print(night_forecast())
     night_forecast = night_forecast()
 
     return render_template('index.html', weather=weather, temp=temp_int, time=time, day_forecast=day_forecast, night_forecast=night_forecast)
+
+
+@app.route('/<string:city>/<string:date>')
+def city_forecast(city, date):
+    API_KEY = '0cecf8365f48c0900489013ff0623926'
+
+    # it explain itself
+    city = city.capitalize()
+    # getting api
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}'
+
+    response = requests.get(url).json()
+
+    weather = {
+        'city': city,
+        'temperature': response['main']['temp'],
+        'humidity': response['main']['humidity'],
+        'wind': response['wind']['speed'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+    }
+
+    # gettiing dict with temperature, date and icon for forecast every 3 hours
+    url_forecast = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={API_KEY}'
+
+    forecast_response = requests.get(url_forecast).json()
+
+    def hour_day_forecast():
+        temp_hour = []
+        for i in forecast_response['list']:
+            foo = date
+            if foo in i['dt_txt']:
+                dictor = {
+                    'date': i['dt_txt'],
+                    'temp': i['main']['temp'],
+                    'icon': i['weather'][0]['icon'],
+                }
+                temp_hour.append(dictor)
+        return temp_hour
+
+    time = strftime('%A %H:%M', localtime())
+    temp_float = weather.get('temperature')
+    temp_int = round(temp_float)
+
+    hour_day_forecast = hour_day_forecast()
+
+    return render_template('city_forecast.html', weather=weather, temp=temp_int, time=time, hour_forecast=hour_day_forecast)
 
 
 if __name__ == '__main__':
